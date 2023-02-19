e841a5b321d9763986d2414a05b453b303ed1e4d
CreatorGao <69323699+CreatorGao@users.noreply.github.com>
2023-02-19 09:17:44 -0500

    Add font

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,13 @@ root.render(
   </React.StrictMode>
 );
 
+export default function Home() {
+  return (
+    <section className="text-gray-600 body-font font-montserrat">
+      ...
+    </section>
+  );
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
