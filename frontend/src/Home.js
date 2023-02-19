e7701aa11044e2bbe0cb6f71ffd6642141c36513
mycoalchen <chen.michael.1116@gmail.com>
2023-02-19 12:14:20 -0500

    + button to add songcard

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import Songcard from "./Songcard";
 
 export default function Home() {
-
+    const [songCards, setSongsCards] = useState([(<Songcard/>), (<Songcard/>)]);
     return (<div className="flex flex-col items-center bg-spotify-darkgray h-screen">
         <div className="sticky top-0 w-screen z-100 text-center p-6 text-4xl bg-black text-white font-bold drop-shadow-spotify-green drop-shadow-lg mb-8">
             Splinify
         </div>
-        <Songcard/>
-        <Songcard/>
+        {songCards}
+        <AddSong/>
     </div>);
+
+    function onAddSong() {
+        let newSongCards = [...songCards];
+        newSongCards.push(<Songcard/>);
+        setSongsCards(newSongCards);
+    }
+    function AddSong() {
+        return (
+            <div>
+                <button onClick={() => onAddSong()} className="text-white font-bold text-4xl">
+                    +
+                </button>
+            </div>
+        )
+    }
 }
