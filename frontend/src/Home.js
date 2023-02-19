222a816785721a07d1ffa4390bd72985da9ea6d7
mycoalchen <chen.michael.1116@gmail.com>
2023-02-19 12:07:31 -0500

    working Spotify API stuff

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,8 +1,9 @@
 import Songcard from "./Songcard";
 
 export default function Home() {
+
     return (<div className="flex flex-col items-center bg-spotify-darkgray h-screen">
-        <div className="sticky top-0 w-screen z-100 text-center p-6 text-4xl bg-black text-white font-bold drop-shadow-spotify-green drop-shadow-lg my-8">
+        <div className="sticky top-0 w-screen z-100 text-center p-6 text-4xl bg-black text-white font-bold drop-shadow-spotify-green drop-shadow-lg mb-8">
             Splinify
         </div>
         <Songcard/>
--- frontend/src/Songcard.js
+++ frontend/src/Songcard.js
@@ -7,7 +7,8 @@ export default function Songcard() {
     const [type, setType] = useState(1);
     const [spotifyToken, setSpotifyToken] = useState("");
     const [title, setTitle] = useState("");
-
+    const [trackId, setTrackId] = useState("");
+    
     const serialize = function (obj) {
         var str = [];
         for (var p in obj) {
@@ -17,7 +18,6 @@ export default function Songcard() {
         }
         return str.join("&");
     }
-
     useEffect(() => {
         var client_id = process.env.REACT_APP_CLIENT_ID;
         var client_secret = process.env.REACT_APP_CLIENT_SECRET;
@@ -38,8 +38,8 @@ export default function Songcard() {
                 if (!error && response.statusCode === 200) {
                     setSpotifyToken(body.access_token);
                 }
-            }).then((resonse) => {
-                console.log(resonse);
+            }).then((response) => {
+                setSpotifyToken(response.data.access_token);
             });
     }, []);
 
@@ -49,14 +49,14 @@ export default function Songcard() {
 
     function handleFindSong() {
         axios.get(`https://api.spotify.com/v1/search?q=track%3A${title}&type=track`, config).then((value) => {
-            console.log(value);
+            setTrackId(value.data.tracks.items[0].id);
         })
     }
 
     return (<form className="text-white px-4 m-4 h-16 w-3/5 bg-black flex flex-row justify-between">
         <input type="text" name="Title" placeholder="Title" className="songCardInput"
             value={title} onChange={(e) => setTitle(e.target.value)} />
-        <input type="button" value="Find song" onClick={() => handleFindSong()} className="bg-spotify-darkgray cursor-pointer" />
+        <input type="button" value="Find song" onClick={() => handleFindSong()} className="bg-spotify-darkgray cursor-pointer my-2 px-2" />
         {/* <input type="text" name="Artist" placeholder="Artist" className="songCardInput"/> */}
     </form>);
 }
