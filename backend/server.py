a98e231726200b5fdcaa92c0fd7f3875781ab7dd
mycoalchen <chen.michael.1116@gmail.com>
2023-02-19 17:45:11 -0500

    fixed backend (for realsies)

--- backend/server.py
+++ backend/server.py
@@ -32,10 +32,12 @@ def service():
         dis = np.array([dance[i],energy[i],valence[i]]).reshape(1,3)
         if(not songs[i] in goodsongs):
             alt = names[knn2.kneighbors(dis, return_distance=False)[0][0]]
-            songs[i]=alt
             alternates[alt]=songs[i]
+            songs[i]=alt
     out = manylink(songs,n,songtoembed,knn,goodsongs)
     print(out)
+    print("WOOOOOO")
+    print(alternates)
 
     for i in range(len(out)):
         if out[i] in alternates.keys():
@@ -46,21 +48,25 @@ def service():
 def playlist(song1, song2, n,songtoembed, knn, goodsongs):
     emb1 = songtoembed[song1]
     emb2 = songtoembed[song2]
-    points = np.array([(x/n)*emb2 +(1-(x/n))*emb1 for x in range(0,n+1)])
+    points = np.array([(x/n)*emb2 +(1-(x/n))*emb1 for x in range(0,n)])
     neighbors = knn.kneighbors(points, return_distance=False)
-    lis = [song1]
+    lis = []
     for arr in neighbors:
         for x in arr:
-            if(not x in lis):
-                lis.append(x)
+            if(not goodsongs[x] in lis):
+                lis.append(goodsongs[x])
                 break
     if(not song2 in lis):
         lis.append(song2)
-    return [goodsongs[x] for x in lis]
+    return lis
 def manylink(arr, n,songtoembed, knn, goodsongs):
     lis = []
     for x in range(len(arr)-1):
-        lis.extend(playlist(arr[x],arr[x+1],n,songtoembed, knn, goodsongs)[:-1])
+        meow = playlist(arr[x],arr[x+1],n,songtoembed, knn, goodsongs)
+        for r in meow:
+            if not r in lis:
+                lis.append(r)
     if(arr[-1] not in lis):
         lis.append(arr[-1])
+    print(lis)
     return lis
