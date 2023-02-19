69caa9365c5cc5d1c95dedc9e63a27901d192e62
mycoalchen <chen.michael.1116@gmail.com>
2023-02-19 09:02:54 -0500

    Setup react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./Home";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Splinify
-        <p>
-          
-        </p>
-        
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
